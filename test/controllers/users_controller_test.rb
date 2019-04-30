76033d9c3bf8873698f37bb6d1bb6ab231b5d502
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-04-29 21:04:33 -0700

    Adding user controller tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,10 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get users_new_url
+  test 'should get new' do
+    get signup_path
     assert_response :success
   end
-
 end
