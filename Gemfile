0ba238cc27dc1337d3796c313ca8771697abcf81
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-05-04 17:17:08 -0700

    Add basic User model and securing passwords

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby '2.6.0'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.3'
+gem 'bcrypt', '~> 3.1.12'
 gem 'bootstrap-sass'
 gem 'rails-controller-testing'
 # Use postgresql as the database for Active Record
---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class User < ApplicationRecord
+  before_save { email.downcase! }
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, length: { minimum: 6 }
+end
